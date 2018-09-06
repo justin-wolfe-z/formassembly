53a5b9b985b61c2efa970be97a7f56a3c10fde2d
justin-wolfe-z <justin.wolfe@zapier.com>
2018-09-06 17:05:23 -0400

    added submission action

--- actions/submitResponse.js
+++ actions/submitResponse.js
@@ -6,11 +6,11 @@ const {
 
 const getFormFields = (z, bundle) => {
   return z
-    .request({
-      url: `https://${bundle.authData.instance_url}/api_v1/forms/view/${
+    .request(
+      `https://${bundle.authData.instance_url}/api_v1/forms/view/${
         bundle.inputData.form
       }.json`
-    })
+    )
     .then(response => {
       const parsedResponse = z.JSON.parse(response.content);
       const fieldsFromFieldsets = processFieldsets(parsedResponse.form);
@@ -28,7 +28,50 @@ const getFormFields = (z, bundle) => {
     });
 };
 
-const submitResponse = (z, bundle) => {};
+const postResponse = (z, bundle) => {
+  return z
+    .request(
+      `https://${bundle.authData.instance_url}/api_v1/forms/view/${
+        bundle.inputData.form
+      }.json`,
+      {
+        method: "GET"
+      }
+    )
+    .then(response => {
+      const parsed = z.JSON.parse(response.content);
+      const outputFields = Object.assign({}, bundle.inputData, {
+        tfa_dbControl: parsed.form.dbControl,
+        tfa_dbFormId: parsed.form.dbFormId
+      });
+      return z
+        .request(
+          `https://${bundle.authData.instance_url}/responses/processor`,
+          {
+            method: "POST",
+            headers: {
+              Accept:
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+              "Accept-Encoding": "gzip, deflate, br",
+              "Accept-Language": "en-US,en;q=0.8",
+              "Cache-Control": "max-age=0",
+              Connection: "keep-alive",
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: outputFields
+          }
+        )
+        .then(response => {
+          if (response.status === 200) {
+            return { status: "success" };
+          } else {
+            throw new Error(
+              "Error submitting form response—please contact support"
+            );
+          }
+        });
+    });
+};
 
 module.exports = {
   key: "submitResponse", // uniquely identifies the action
@@ -50,6 +93,6 @@ module.exports = {
       },
       getFormFields
     ],
-    perform: submitResponse
+    perform: postResponse
   }
 };
